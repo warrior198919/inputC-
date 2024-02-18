f976d6db250d02942119e479a50bd1f1045fc5b6
Aleksey Graunov <agraunov@gmail.com>
2024-02-18 03:59:40 +0300

    add new operator

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+int i = 0;
+
+while (i < 10)
+{
+    i += 2;
+    Console.Write(i);
+    Console.Write(' ');
+}
